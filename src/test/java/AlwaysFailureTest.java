0ca83c8c014acefac828ff60bcaf939e1e7eb084
Naruhiko Ogasawara <naruoga@gmail.com>
2017-12-18 19:57:49 +0900

    AlwaysFailureTest: 再度無視

--- src/test/java/AlwaysFailureTest.java
+++ src/test/java/AlwaysFailureTest.java
@@ -1,4 +1,3 @@
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -6,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlwaysFailureTest {
 
+    @Disabled
     @Test
     void 常に失敗するテスト() {
         assertEquals(1 * 1, 2);
